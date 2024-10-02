3757fc4573d4c31b07f3231cbf77ec10e28de68a
Thibaut Haberer <thibaut.haberer@etu.univ-grenoble-alpes.fr>
2024-10-02 09:26:32 +0200

    update test

--- task1/src/task1/Test.java
+++ task1/src/task1/Test.java
@@ -5,10 +5,11 @@ public class Test {
 	static final String SERVER_NAME = "server";
 	static final String CLIENT_NAME = "client";
 	static final int PORT = 123;
+	static final BrokerImplem server = new BrokerImplem(SERVER_NAME);
+	static final BrokerImplem client = new BrokerImplem(CLIENT_NAME);
 
 	public static void main(String[] args) {
-		BrokerImplem server = new BrokerImplem(SERVER_NAME);
-		BrokerImplem client = new BrokerImplem(CLIENT_NAME);
+		
 
 		Task serverTask = new Task(server, () -> {
 			while (true) {
@@ -18,9 +19,8 @@ public class Test {
 					int n = 0;
 					while (n >= 0) {
 						n = channel.read(buffer, 0, buffer.length);
-						if (n > 0) {
-							channel.write(buffer, 0, n);
-						}
+						int nw=channel.write(buffer, 0, n);
+						assert(n==nw);
 					}
 				} catch (DisconnectedException e) {
 					channel.disconnect();
@@ -31,47 +31,40 @@ public class Test {
 		});
 		serverTask.start();
 
-		Task[] clientTasks = new Task[5];
-		for (int j = 0; j < 5; j++) {
-			clientTasks[j] = new Task(client, () -> {
-				try {
-					Channel channel = client.connect(SERVER_NAME, PORT);
-				byte[] buffer = new byte[256];
-				for (int i = 0; i < 256; i++) {
-					buffer[i] = (byte) i;
-				}
-				int nwrite = channel.write(buffer, 0, buffer.length);
-				assert (nwrite == buffer.length);
-	
-				byte[] verifbuffer = new byte[256];
-				int offset = 0;
-				int n = 0;
-				while (n >= 0) {
-					n = channel.read(verifbuffer, offset, verifbuffer.length);
-					offset += n;
-				}
-	
-				for (int i = 0; i < 256; i++) {
-					assert (buffer[i] == verifbuffer[i]);
-				}
-	
-				channel.disconnect();
-				assert (channel.disconnected());
-				} catch (DisconnectedException e) {
-					System.out.println("Client disconnected Exception");
-				}
-				System.out.println("Client task done");
-				
-			});
-			clientTasks[j].start();
-		}
-
-		for (int i = 0; i < 5; i++) {
-			try {
-				clientTasks[i].join();
-			} catch (InterruptedException e) {
-				e.printStackTrace();
+		Task clientTasks = new Task(client, () -> {
+		try {
+			Channel channel = client.connect(SERVER_NAME, PORT);
+			byte[] buffer = new byte[256];
+			for (int i = 0; i < 256; i++) {
+				buffer[i] = ((byte) i);
 			}
+			int n=0;
+			byte[] verifbuffer = new byte[256];
+			int offset = 0;
+			while(offset<buffer.length) {
+				n=channel.write(buffer, 0, buffer.length-offset);
+				offset += channel.read(verifbuffer, offset, n);
+			}
+
+			for (int i = 0; i < 256; i++) {
+				//probleme a partir de 128
+				assert (buffer[i] == verifbuffer[i]);
+			}
+	
+			channel.disconnect();
+			assert (channel.disconnected());
+			} catch (DisconnectedException e) {
+				System.out.println("Client disconnected Exception");
+			}
+			System.out.println("Client task done");
+				
+		});
+		clientTasks.start();
+		System.out.print("Client and Server started");
+		try {
+			clientTasks.join();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
 		}
 		serverTask.interrupt();
 
